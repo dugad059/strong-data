6a95fd42b70e8d18f33d260aacf1ec54a8a51cf6
David Dugas <daviddugas@Davids-MacBook-Pro.local>
2022-08-11 17:16:05 -0500

    WIP: functions created

---
+++ csv_import_functions.py
@@ -0,0 +1,123 @@
+import os
+import pandas as pd
+import psycopg2
+
+# -------------------------------------------------------------
+# Find CSV files in my current working directory
+# Isolate only the CSV Files
+def csv_files():
+    csv_files = []
+    for file in os.listdir(os.getcwd()):
+        if file.endswith('.csv'):
+            csv_files.append(file)
+    return csv_files
+# ----------------------------------------------------------------
+
+# Create a new directory for the CSV files 
+def configure_dataset_directory(csv_files, dataset_dir):
+
+    # Make directory bash command
+    try:
+        mkdir = 'mkdir {0}'.format(dataset_dir)
+        os.system(mkdir)
+    except:
+        pass
+
+    # Move files to new directory
+    for csv in csv_files:
+        mv_file = "mv '{0}' {1}".format(csv, dataset_dir)
+        os.system(mv_file)
+        
+    
+    return
+
+# -----------------------------------------------------------------------
+
+#Create the pandas df from the CSV files to be able to transform them
+def create_df(dataset_dir, csv_files):
+    
+    # Path to the CSV files
+    data_path = os.getcwd()+'/'+dataset_dir+'/'
+
+    # Loop through the files and create the dataframe
+    df = {}
+    for file in csv_files:
+        try:
+            df[file] = pd.read_csv(data_path+file)
+        except UnicodeDecodeError:
+            df[file] = pd.read_csv(data_path+file, encoding='ISO-8859-1')
+            # This is a comment error, so this except will help by pass it! It's not an error we need to worry about! If there is an error in the encoding it will use that encoding on the pandas df 
+    
+    return df
+
+# -----------------------------------------------------------------------
+
+# Cleans table and column names for postgre
+def clean_tbl_name(filename):
+
+    clean_tbl_name = filename.lower().replace(" ","_").replace("?", '').replace("-","_").replace(r"/","_").replace("\\","_").replace("%","").replace(")","").replace(r"(","").replace("&","")
+
+    # Removes .csv from the clean_tbl_name
+    tbl_name = '{0}'.format(clean_tbl_name.split('.')[0])
+    
+    return tbl_name
+
+# -----------------------------------------------------------------------
+
+def clean_colname(dataframe):
+
+ # Cleans table and column names for postgre 
+    dataframe.columns = [x.lower().replace(" ","_").replace("?", '').replace("-","_").replace(r"/","_").replace("\\","_").replace("%","").replace(")","").replace(r"(","").replace("&","") for x in dataframe.columns] 
+
+    # Replacement dictionary that maps pandds dtypes to sql dtypes
+    replacements = {
+        'object' : 'varchar',
+        'float64' : 'float',
+        'int64' : 'int',
+        'datetime64' : 'timestamp',
+        'timedelta64[ns]' : 'varchar' 
+    }
+
+    #Table Schema
+    col_str = ', '.join('{} {}'.format(n,d) for (n,d) in zip(dataframe.columns, dataframe.dtypes.replace(replacements)))
+
+    return col_str, dataframe.columns
+
+# -----------------------------------------------------------------------------------------------
+ # Connection to DB
+def upload_to_db(host, dbname, user,password, tbl_name, col_str, file, dataframe, dataframe_columns):
+    # Opens a database connection
+    conn_string = f"host={host} dbname={dbname} user={user} password={password}"
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    print('open db success')
+
+    # Drops table with the same name
+    cursor.execute(f'drop table if exists {tbl_name}')
+
+    # Creates table
+    cursor.execute(f'create table {tbl_name} ({col_str})')
+    print(f'{tbl_name} was created successfully')
+
+    # Saved pandas df back to a csv
+    dataframe.to_csv(file, header=dataframe_columns, index=False, encoding='utf-8')
+
+    # Opens the csv file and saves it as an object
+    my_file = open(file)
+
+    # Uploads CSV to db
+    SQL_STATEMENT = '''
+    COPY %s FROM STDIN WITH
+        CSV
+        HEADER
+        DELIMITER AS ','
+    '''
+
+    cursor.copy_expert(sql=SQL_STATEMENT % tbl_name, file=my_file)
+    print('file copied to db')
+
+    cursor.execute(f'grant select on table {tbl_name} to public')
+    conn.commit()
+
+    cursor.close()
+    print(f'table {tbl_name} imported to db complete')
--- main.py
+++ main.py
@@ -1,95 +1,33 @@
-import os
-import pandas as pd
-import numpy as np
-import psycopg2
+from csv_import_functions import *
 
-csv_files = []
-for file in os.listdir(os.getcwd()):
-    if file.endswith('.csv'):
-        csv_files.append(file)
 
+# DB settings
+host = 'strong-data.cnzuwg8ioub5.us-east-1.rds.amazonaws.com'
+dbname = 'postgres'
+user = 'daviddugas'
+password = '20Duffle$'
+
+#Settings
 dataset_dir = 'datasets'
 
-try:
-    mkdir = 'mkdir {0}'.format(dataset_dir)
-    os.system(mkdir)
-except:
-    pass
-
-for csv in csv_files:
-    mv_file = "mv '{0}' {1}".format(csv, dataset_dir)
-    os.system(mv_file)
-
-
-data_path = os.getcwd()+'/'+dataset_dir+'/'
-
-df = {}
-for file in csv_files:
-    try:
-        df[file] = pd.read_csv(data_path+file)
-    except UnicodeDecodeError:
-        df[file] = pd.read_csv(data_path+file, encoding='ISO-8859-1')
-   
-
+# Configure Environment and create main df
+csv_files = csv_files()
+configure_dataset_directory(csv_files, dataset_dir)
+df = create_df(dataset_dir, csv_files)
 
 for k in csv_files:
+    
+    # Call Dataframe
     dataframe = df[k]
-
-    clean_tbl_name = k.lower().replace(" ","_").replace("?", '').replace("-","_").replace(r"/","_").replace("\\","_").replace("%","").replace(")","").replace(r"(","").replace("&","")
-
-
-
-    tbl_name = '{0}'.format(clean_tbl_name.split('.')[0])
-  
-
-    dataframe.columns = [x.lower().replace(" ","_").replace("?", '').replace("-","_").replace(r"/","_").replace("\\","_").replace("%","").replace(")","").replace(r"(","").replace("&","") for x in dataframe.columns] 
-
-    replacements = {
-        'object' : 'varchar',
-        'float64' : 'float',
-        'int64' : 'int',
-        'datetime64' : 'timestamp',
-        'timedelta64[ns]' : 'varchar' 
-    }
-
-    col_str = ', '.join('{} {}'.format(n,d) for (n,d) in zip(dataframe.columns, dataframe.dtypes.replace(replacements)))
    
+    # Clean table name
+    tbl_name = clean_tbl_name(k)
+   
+    # Clean column names
+    col_str, dataframe.columns = clean_colname(dataframe) 
 
-    host = 'strong-data.cnzuwg8ioub5.us-east-1.rds.amazonaws.com'
-    dbname = 'postgres'
-    user = 'daviddugas'
-    password = '20Duffle$'
-
-    conn_string = f"host={host} dbname={dbname} user={user} password={password}"
+    # Uploads Data to DB
+    upload_to_db(host, dbname, user,password, tbl_name, col_str,file=k, dataframe=dataframe , dataframe_columns=dataframe.columns)
 
 
-    conn = psycopg2.connect(conn_string)
-    cursor = conn.cursor()
-    print('open db success')
 
-    cursor.execute(f'drop table if exists {tbl_name}')
-
-    cursor.execute(f'create table {tbl_name} ({col_str})')
-    print(f'{tbl_name} was created successfully')
-
-    dataframe.to_csv(k, header=dataframe.columns, index=False, encoding='utf-8')
-
-    my_file = open(k)
-
-    SQL_STATEMENT = '''
-    COPY %s FROM STDIN WITH
-        CSV
-        HEADER
-        DELIMITER AS ','
-    '''
-
-    cursor.copy_expert(sql=SQL_STATEMENT % tbl_name, file=my_file)
-    print('file copied to db')
-
-    cursor.execute(f'grant select on table {tbl_name} to public')
-    conn.commit()
-
-    cursor.close()
-    print(f'table {tbl_name} imported to db complete')
-
-print('All table have been successfully importing into the db')
