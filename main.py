9e78d6bda7287d2dd48e48cdf1b8889cc1dd69fe
David Dugas <daviddugas@Davids-MacBook-Pro.local>
2022-08-11 12:11:58 -0500

    WIP: automation complete

--- main.py
+++ main.py
@@ -1,62 +1,95 @@
 import os
 import pandas as pd
+import numpy as np
 import psycopg2
 
+csv_files = []
+for file in os.listdir(os.getcwd()):
+    if file.endswith('.csv'):
+        csv_files.append(file)
 
-df = pd.read_csv('strong.csv')
-# print(df.head())
+dataset_dir = 'datasets'
 
-file = 'strong'
+try:
+    mkdir = 'mkdir {0}'.format(dataset_dir)
+    os.system(mkdir)
+except:
+    pass
 
-clean_tbl_name = file.lower().replace(" ","_").replace("?", '').replace("-","_").replace(r"/","_").replace("\\","_").replace("%","").replace(")","").replace(r"(","").replace("&","")
+for csv in csv_files:
+    mv_file = "mv '{0}' {1}".format(csv, dataset_dir)
+    os.system(mv_file)
 
-# print(clean_tbl_name)
 
-df.columns = [x.lower().replace(" ","_").replace("?", '').replace("-","_").replace(r"/","_").replace("\\","_").replace("%","").replace(")","").replace(r"(","").replace("&","") for x in df.columns]
+data_path = os.getcwd()+'/'+dataset_dir+'/'
 
-# print(df.columns)
+df = {}
+for file in csv_files:
+    try:
+        df[file] = pd.read_csv(data_path+file)
+    except UnicodeDecodeError:
+        df[file] = pd.read_csv(data_path+file, encoding='ISO-8859-1')
+   
 
-replacements = {
-    'object' : 'varchar',
-    'float64' : 'float',
-    'int64' : 'int',
-    'datetime64' : 'timestamp',
-    'timedelta64[ns]' : 'varchar' 
-}
 
-# print(replacements)
+for k in csv_files:
+    dataframe = df[k]
 
-col_str = ', '.join('{} {}'.format(n,d) for (n,d) in zip(df.columns, df.dtypes.replace(replacements)))
+    clean_tbl_name = k.lower().replace(" ","_").replace("?", '').replace("-","_").replace(r"/","_").replace("\\","_").replace("%","").replace(")","").replace(r"(","").replace("&","")
 
-# print(col_str)
 
-conn_string = "host=strong-data.cnzuwg8ioub5.us-east-1.rds.amazonaws.com dbname='postgres' user='daviddugas' password='20Duffle$'"
 
-conn = psycopg2.connect(conn_string)
-cursor = conn.cursor()
-print('open db success')
+    tbl_name = '{0}'.format(clean_tbl_name.split('.')[0])
+  
 
-cursor.execute('drop table if exists strong')
+    dataframe.columns = [x.lower().replace(" ","_").replace("?", '').replace("-","_").replace(r"/","_").replace("\\","_").replace("%","").replace(")","").replace(r"(","").replace("&","") for x in dataframe.columns] 
 
-cursor.execute('create table strong (date varchar, workout_name varchar, duration varchar, exercise_name varchar, set_order int, weight float, reps int, distance int, seconds int, notes varchar, workout_notes float, rpe float)')
+    replacements = {
+        'object' : 'varchar',
+        'float64' : 'float',
+        'int64' : 'int',
+        'datetime64' : 'timestamp',
+        'timedelta64[ns]' : 'varchar' 
+    }
 
-df.to_csv('strong.csv', header=df.columns, index=False, encoding='utf-8')
+    col_str = ', '.join('{} {}'.format(n,d) for (n,d) in zip(dataframe.columns, dataframe.dtypes.replace(replacements)))
+   
 
-my_file = open('strong.csv')
-print('file opened')
+    host = 'strong-data.cnzuwg8ioub5.us-east-1.rds.amazonaws.com'
+    dbname = 'postgres'
+    user = 'daviddugas'
+    password = '20Duffle$'
 
-SQL_STATEMENT = '''
-COPY strong FROM STDIN WITH
-CSV
-HEADER
-DELIMITER AS ','
-'''
+    conn_string = f"host={host} dbname={dbname} user={user} password={password}"
 
-cursor.copy_expert(sql=SQL_STATEMENT, file=my_file)
-print('file copied to db')
 
-cursor.execute('grant select on table strong to public')
-conn.commit()
+    conn = psycopg2.connect(conn_string)
+    cursor = conn.cursor()
+    print('open db success')
 
-cursor.close()
-print('table strong imported to db complete')
+    cursor.execute(f'drop table if exists {tbl_name}')
+
+    cursor.execute(f'create table {tbl_name} ({col_str})')
+    print(f'{tbl_name} was created successfully')
+
+    dataframe.to_csv(k, header=dataframe.columns, index=False, encoding='utf-8')
+
+    my_file = open(k)
+
+    SQL_STATEMENT = '''
+    COPY %s FROM STDIN WITH
+        CSV
+        HEADER
+        DELIMITER AS ','
+    '''
+
+    cursor.copy_expert(sql=SQL_STATEMENT % tbl_name, file=my_file)
+    print('file copied to db')
+
+    cursor.execute(f'grant select on table {tbl_name} to public')
+    conn.commit()
+
+    cursor.close()
+    print(f'table {tbl_name} imported to db complete')
+
+print('All table have been successfully importing into the db')
